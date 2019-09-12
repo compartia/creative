9236e6eed43d56749940803eae570ea5ce61a862
Artem Zaborskiy <compartia@gmail.com>
2019-09-12 17:24:34 +0800

    rotating stars

--- stars.processing.js
+++ stars.processing.js
@@ -7,51 +7,41 @@ boolean looping = true;
 void setup() {
   size(640, 640);
   background(255);
-  frameRate(30);
+  frameRate(20);
 }
 
+phase = 0;
+sradius = 50
+time = 0;
 void draw() {
-  a_change += PI/112.0;
-
+	time += 0.1
+  a_change += PI/212.0;
+	phase = (sin(time/300.0)+1)*2
+	sradius = 50 * cos(time/200.0)
+	 
   background(0); 
 
-  fill(255); 
+  noFill(); 
   stroke(255); 
 	
   // vary the width - start just off the screen so we don't have black edges
   for (float centy = -50+6*radius/4; centy < width+100; centy += radius*3) { // offset three times to skip a row
     for (float centx = 0; centx < width+100; centx += sqrt(3)/2 * 2*radius) {
-			draw_star(centx, centy, radius, 0 )
-     //  beginShape();
-     //  //fill(0);
-     //  for (float a = PI/6; a < TWO_PI; a += angle) {
-     //    float vx = centx + cos(a + a_change) * radius;
-     //    float vy = centy + sin(a + a_change) * radius;
-     //    vertex(vx, vy);
-     // };
-     // endShape(CLOSE);
+			draw_star(centx, centy, sradius, phase )
     };
   }
 
   for (float centy = -50; centy < height+100; centy+= radius*3) {
     for (float centx = sqrt(3)/2 * radius; centx < width+100; centx += sqrt(3)/2 * 2*radius) {
-     //  beginShape();
-     //  //fill(0);
-     //  for (float a = PI/6; a < TWO_PI; a += angle) {
-     //    float vx = centx + cos(a + a_change) * radius;
-     //    float vy = centy + sin(a + a_change) * radius;
-     //    vertex(vx, vy);
-     // };
-     //  endShape(CLOSE);
-			draw_star(centx, centy, radius, 0 )
+			draw_star(centx, centy, sradius, phase )
     }
   }
 }
 
 void draw_star(x, y, radius, phase){
 	beginShape();
-	fill(0);
-	rsmall =  radius/3.0
+	// fill(0);
+	rsmall =  radius*phase
 	for (float a = PI/6; a < TWO_PI; a += angle) {
 		float vx = x + cos(a + a_change) * radius;
 		float vy = y + sin(a + a_change) * radius;
