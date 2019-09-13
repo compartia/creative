8687a5ccf8f89b189f01e8733c990a8aa1f9b3c1
Artem Zaborskiy <compartia@gmail.com>
2019-09-13 16:23:19 +0800

    Update stars.processing.js

--- stars.processing.js
+++ stars.processing.js
@@ -7,55 +7,54 @@ boolean looping = true;
 void setup() {
   size(640, 640);
   background(255);
-  frameRate(30);
+  frameRate(60);
 }
 
-base_r=100
-phase = 0.333;
-sradius = base_r
-time = 0;
+int base_r=100;
+float phase = 0.333;
+float sradius = base_r;
+float time = 0;
 
 void draw() {
-	time += 0.005
+  time += 0.005f;
   a_change += PI/400;
-	phase = (sin(time)+1)/2
-	sradius = base_r * cos(time/3.0)
-	 
-  background(0); 
+  phase = (sin(time)+1)/2f;
+  sradius = base_r * cos(time/3.0);
+   
+  //background(0); 
 
-  
   stroke(255, 140); 
-	
+  
   // vary the width - start just off the screen so we don't have black edges
   for (float centy = -50+6*radius/4; centy < width+100; centy += radius*3) { // offset three times to skip a row
     for (float centx = 0; centx < width+100; centx += sqrt(3)/2 * 2*radius) {
-			draw_star(centx, centy, sradius, phase )
+      draw_star(centx, centy, sradius, phase);
     };
   }
 
   for (float centy = -50; centy < height+100; centy+= radius*3) {
     for (float centx = sqrt(3)/2 * radius; centx < width+100; centx += sqrt(3)/2 * 2*radius) {
-			draw_star(centx, centy, sradius, phase )
+      draw_star(centx, centy, sradius, phase);
     }
   }
 }
 
-void draw_star(x, y, radius, phase){
-	noFill(); 
-	beginShape();
-	fill(2,phase,0, 100);
-	rsmall =  radius * phase
-	rot = a_change - angle/4
-	for (float a = PI/6; a < TWO_PI; a += angle) {
-		float vx = x + cos(a + rot) * radius;
-		float vy = y + sin(a + rot) * radius;
-		vertex(vx, vy);
-		
-		vx = x + cos(a + rot + angle/2) * rsmall;
-		vy = y + sin(a + rot + angle/2) * rsmall;
-		vertex(vx, vy);
+void draw_star(float x, float y, float radius, float phase){
+  noFill(); 
+  beginShape();
+  fill(2, phase, 0, 1);
+  float rsmall =  radius * phase;
+  float  rot = a_change - angle/4;
+  for (float a = PI/6; a < TWO_PI; a += angle) {
+    float vx = x + cos(a + rot) * radius;
+    float vy = y + sin(a + rot) * radius;
+    vertex(vx, vy);
+    
+    vx = x + cos(a + rot + angle/2) * rsmall;
+    vy = y + sin(a + rot + angle/2) * rsmall;
+    vertex(vx, vy);
   };
-	endShape(CLOSE);
+  endShape(CLOSE);
 }
 
 
