327ae5025f7970eecc87beb6384d9e5565396c17
Artem Zaborskiy <compartia@gmail.com>
2019-09-13 09:10:55 +0800

    trails

--- stars.processing.js
+++ stars.processing.js
@@ -7,22 +7,24 @@ boolean looping = true;
 void setup() {
   size(640, 640);
   background(255);
-  frameRate(20);
+  frameRate(30);
 }
 
-phase = 0;
-sradius = 50
+base_r=100
+phase = 0.333;
+sradius = base_r
 time = 0;
+
 void draw() {
-	time += 0.1
-  a_change += PI/212.0;
-	phase = (sin(time/300.0)+1)*2
-	sradius = 50 * cos(time/200.0)
+	time += 0.005
+  a_change += PI/400;
+	phase = (sin(time)+1)/2
+	sradius = base_r * cos(time/3.0)
 	 
   background(0); 
 
-  noFill(); 
-  stroke(255); 
+  
+  stroke(255, 140); 
 	
   // vary the width - start just off the screen so we don't have black edges
   for (float centy = -50+6*radius/4; centy < width+100; centy += radius*3) { // offset three times to skip a row
@@ -39,16 +41,18 @@ void draw() {
 }
 
 void draw_star(x, y, radius, phase){
+	noFill(); 
 	beginShape();
-	// fill(0);
-	rsmall =  radius*phase
+	fill(2,phase,0, 100);
+	rsmall =  radius * phase
+	rot = a_change - angle/4
 	for (float a = PI/6; a < TWO_PI; a += angle) {
-		float vx = x + cos(a + a_change) * radius;
-		float vy = y + sin(a + a_change) * radius;
+		float vx = x + cos(a + rot) * radius;
+		float vy = y + sin(a + rot) * radius;
 		vertex(vx, vy);
 		
-		vx = x + cos(a + a_change + angle/2) * rsmall;
-		vy = y + sin(a + a_change + angle/2) * rsmall;
+		vx = x + cos(a + rot + angle/2) * rsmall;
+		vy = y + sin(a + rot + angle/2) * rsmall;
 		vertex(vx, vy);
   };
 	endShape(CLOSE);
