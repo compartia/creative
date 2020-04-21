ae1bcca7a6e5dd70660293742759781cd8a53511
Artem Zaborskiy <compartia@gmail.com>
2020-04-21 16:21:32 +0800

    More...

--- snowflakes/paper.js/shocode.js
+++ snowflakes/paper.js/shocode.js
@@ -1,61 +1,81 @@
- 
+
 const BASE = 6;
 
-function color(r, g, b){
-    return new Color(r/255.,g/255.,b/255.);
+function color(r, g, b) {
+    return new Color(r / 255.0, g / 255.0, b / 255.0);
 }
 const pal = [
-  color(193,24,39),
-  color(255,227,0),
-  color(31,75,149),
-  
-  color(174,185,184),
-  color(239,237,220),
-  color(165,213,236),
+    color(193, 24, 39),
+    color(255, 227, 0),
+    color(31, 75, 149),
+
+    color(174, 185, 184),
+    color(239, 237, 220),
+    color(165, 213, 236),
 ];
 
 
-function drawPixel(r, value, bits_pal){
+function drawPixel(r, value, bits_pal) {
 
     var th = r * 0.38;
-    var angle_deg  = 90 + value * 60 ; 
-  
-//   rotate(angle);
+    var angle_deg = 90 + value * 60;
+
+    //   rotate(angle);
 
     var path = new Path();
-    path.add(new Point(-th/2, -r/2)); 
-    path.add(new Point( th/2, -r/2)); 
-    path.add(new Point( th/2, r/2)); 
-    path.add(new Point(-th/2, r/2)); 
+    path.add(new Point(-th / 2, -r / 2));
+    path.add(new Point(th / 2, -r / 2));
+    path.add(new Point(th / 2, r / 2));
+    path.add(new Point(-th / 2, r / 2));
     path.closed = true;
-    path.fillColor =   bits_pal[  (value) % bits_pal.length  ] ;
-//   noStroke();
-//   fill( bits_pal[  (value) % bits_pal.length  ] );
- 
-//   endShape(CLOSE);
+    path.fillColor = bits_pal[(value) % bits_pal.length];
+    //   noStroke();
+    //   fill( bits_pal[  (value) % bits_pal.length  ] );
+
+    //   endShape(CLOSE);
 
 
-//   popMatrix();
-//   popStyle();
+    //   popMatrix();
+    //   popStyle();
     path.rotate(angle_deg);
     return path;
 }
 
 const r_legend = 30;
-for (var i=0; i<BASE; i++){
-    pixel = drawPixel(r_legend/2, i, pal);  
-    pixel.position = new Point(r_legend*(2+i), r_legend*2);
+for (var i = 0; i < BASE; i++) {
+    pixel = drawPixel(r_legend / 2, i, pal);
+    pixel.position = new Point(r_legend * (2 + i), r_legend * 2);
 }
 
+const hex_aspect = Math.sin(2.0 * Math.PI / 3.0);
 
-var circlePath = new Path.Circle(new Point(280, 100), 25);
-circlePath.strokeColor = 'black';
-circlePath.fillColor = 'white';
+const _digits = [1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0]
+const R = 200;
 
-var clones = 30;
-var angle = 360 / clones;
+function drawSectorHalf(digits, bits_pal) {
+    const rd = R / 15.0;
 
-for(var i = 0; i < clones; i++) {
-	var clonedPath = circlePath.clone();
-	clonedPath.rotate(angle * i, circlePath.bounds.topLeft);
-};
+    var n = 0;
+
+    for (var row = 0; row < 30; row++) {
+        for (var j = row / 2; j < row; j++) {
+            if (n < digits.length) {
+
+                const ci = digits[n]; //digit corresponds to color and rotation 
+
+                // following the grid
+                var x = rd + j * rd - rd * (row % 2) * 0.5 - rd * Math.ceil(row / 2);
+                var y = hex_aspect * (row * rd);
+
+
+                pixel = drawPixel(rd, ci, bits_pal, false);
+                pixel.position = new Point(x + 200, y + 200);
+            }
+            n++;
+        }
+
+    }
+
+}
+
+drawSectorHalf(_digits, pal) 
